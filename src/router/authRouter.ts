23c8b86b3de714edb19513b1a69bac67c455861a
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-09-07 17:00:23 +0300

    joi validation

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -4,8 +4,8 @@ import { authMiddleware, userMiddleware } from '../middlewares';
 
 const router = Router();
 
-router.post('/registration', authController.registration);
-router.post('/login', userMiddleware.checkIsUserExists, authController.login);
+router.post('/registration', userMiddleware.checkIsUserValid, authController.registration);
+router.post('/login', userMiddleware.checkIsMailAndPasswordValid, userMiddleware.checkIsUserExists, authController.login);
 router.post('/logout', authMiddleware.checkAccessToken, authController.logout);
 router.post('/refresh', authMiddleware.checkRefreshToken, authController.refreshToken);
 
